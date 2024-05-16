1789dd30d53b987a5d75826f0f501e937fb9c1f0
DiegoN <dnavarro@araujoysegovia.com>
2024-05-16 09:57:06 -0500

    first commit

---
+++ CafeJavaApplication26/src/cafejavaapplication26/CafeJavaApplication26.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cafejavaapplication26;
+
+/**
+ *
+ * @author lenovo
+ */
+public class CafeJavaApplication26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
